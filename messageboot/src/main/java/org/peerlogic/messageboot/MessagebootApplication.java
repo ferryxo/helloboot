f47fb32182205ac2e3fbdc716c21ac8231ece48a
ferry <ferryxo@gmail.com>
2018-08-10 10:30:10 -0400

    initial

---
+++ messageboot/src/main/java/org/peerlogic/messageboot/MessagebootApplication.java
@@ -0,0 +1,12 @@
+package org.peerlogic.messageboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagebootApplication.class, args);
+	}
+}
